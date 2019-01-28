c25bec55061d18fc9df309de412d85ccab464a74
SC <scavazose@gmail.com>
2019-01-27 21:56:03 -0600

    Heroku MySql SetUp

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'sercav1575',
+    database: 'burgers_db'
+  });
+};
+/* var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -14,6 +26,6 @@ connection.connect(function(err) {
     return;
   }
   console.log("connected as id " + connection.threadId);
-});
-
+}); */
+connection.connect();
 module.exports = connection;
